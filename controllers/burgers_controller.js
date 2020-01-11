b222caee25b1f9d87ea2c32c848fe71400e19938
Caitlan Jeffrey <caitlanjeffrey@gmail.com>
2020-01-11 13:36:18 -0800

    adding index handlebar information

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var burger = require("../models/cat.js");
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
